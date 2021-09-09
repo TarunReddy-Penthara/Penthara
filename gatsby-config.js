bbbc664771305a872b722c100100de893f441745
Tarun Reddy <tarun.reddy@penthara.com>
2021-09-09 18:40:26 +0530

    merge

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,20 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `Services`,
+        path: `${__dirname}/src/content/services/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `pages`,
         path: `${__dirname}/src/pages/`,
       },
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-netlify-cms`
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
   ],
 };
--- src/content/services/digital-workplace-consulting.md
+++ src/content/services/digital-workplace-consulting.md
@@ -5,8 +5,6 @@ description: Using Microsoft 365 platform our experts deliver end to end
   solutions that drive productivity, effective communication, enhanced
   collaboration and automation.
 ---
-<!--StartFragment-->
 
 Using Microsoft 365 platform our experts deliver end to end solutions that drive productivity, effective communication, enhanced collaboration and automation.
 
-<!--EndFragment-->
